c33bbe5de9da9181ca8169ef73222fddd51863e3
MalaakJaludi <121072340+Malaak12@users.noreply.github.com>
2024-05-01 23:30:51 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        flagPictureBox = New PictureBox()
+        Label1 = New Label()
+        OptionsComboBox = New ComboBox()
+        SubmitButton = New Button()
+        NextButton = New Button()
+        Label2 = New Label()
+        Label3 = New Label()
+        FeedbackLabel = New Label()
+        GroupBox1.SuspendLayout()
+        CType(flagPictureBox, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(flagPictureBox)
+        GroupBox1.Location = New Point(29, 32)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(268, 200)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Flag"
+        ' 
+        ' flagPictureBox
+        ' 
+        flagPictureBox.Location = New Point(36, 49)
+        flagPictureBox.Name = "flagPictureBox"
+        flagPictureBox.Size = New Size(200, 122)
+        flagPictureBox.SizeMode = PictureBoxSizeMode.Zoom
+        flagPictureBox.TabIndex = 1
+        flagPictureBox.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(357, 32)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(175, 32)
+        Label1.TabIndex = 1
+        Label1.Text = "Select Country:"
+        ' 
+        ' OptionsComboBox
+        ' 
+        OptionsComboBox.FormattingEnabled = True
+        OptionsComboBox.Items.AddRange(New Object() {"australia", "brazil", "china", "italy", "russia", "southafria", "spain", "unitedstates"})
+        OptionsComboBox.Location = New Point(357, 81)
+        OptionsComboBox.Name = "OptionsComboBox"
+        OptionsComboBox.Size = New Size(242, 40)
+        OptionsComboBox.TabIndex = 2
+        ' 
+        ' SubmitButton
+        ' 
+        SubmitButton.Location = New Point(689, 47)
+        SubmitButton.Name = "SubmitButton"
+        SubmitButton.Size = New Size(150, 60)
+        SubmitButton.TabIndex = 3
+        SubmitButton.Text = "Submit"
+        SubmitButton.UseVisualStyleBackColor = True
+        ' 
+        ' NextButton
+        ' 
+        NextButton.Location = New Point(689, 146)
+        NextButton.Name = "NextButton"
+        NextButton.Size = New Size(150, 63)
+        NextButton.TabIndex = 4
+        NextButton.Text = "Next Flag"
+        NextButton.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(357, 161)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(0, 32)
+        Label2.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 28.125F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(357, 146)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(0, 100)
+        Label3.TabIndex = 6
+        ' 
+        ' FeedbackLabel
+        ' 
+        FeedbackLabel.AutoSize = True
+        FeedbackLabel.Location = New Point(363, 146)
+        FeedbackLabel.Name = "FeedbackLabel"
+        FeedbackLabel.Size = New Size(14, 32)
+        FeedbackLabel.TabIndex = 7
+        FeedbackLabel.Text = vbCrLf
+        FeedbackLabel.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(876, 286)
+        Controls.Add(FeedbackLabel)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(NextButton)
+        Controls.Add(SubmitButton)
+        Controls.Add(OptionsComboBox)
+        Controls.Add(Label1)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Flag Game"
+        GroupBox1.ResumeLayout(False)
+        CType(flagPictureBox, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents OptionsComboBox As ComboBox
+    Friend WithEvents SubmitButton As Button
+    Friend WithEvents NextButton As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents FeedbackLabel As Label
+    Friend WithEvents flagPictureBox As PictureBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,98 @@
+﻿Public Class Form1
+    Inherits System.Windows.Forms.Form
+
+    Dim m_strOptions As String() = {"Australia", "Brazil", "China", "Italy", "Russia", "South Africa", "Spain", "United States"}
+    Dim m_blnUsed As Boolean() = New Boolean(m_strOptions.GetUpperBound(0)) {}
+
+    Dim m_intCount As Integer = 1  ' number of flags shown
+    Dim m_strCountry As String     ' current flag's country
+
+    ' handles Flag Quiz Form's Load event
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Array.Sort(m_strOptions)  ' alphabetize country names
+        ' display country names in ComboBox
+        OptionsComboBox.DataSource = m_strOptions
+        DisplayFlag()  ' display first flag in PictureBox
+    End Sub ' Form1_Load
+
+    ' return full path name of images file as a String
+    Function BuildPathName(country As String) As String
+        ' begin with country name
+        Dim strOutput As String = country
+        ' remove spaces from country name
+        strOutput = strOutput.Replace(" ", "")
+        strOutput &= ".png" ' add file extension
+        ' add path name
+        strOutput = System.IO.Path.Combine(Application.StartupPath, "images\", strOutput)
+        Return strOutput  ' return full path name
+    End Function ' BuildPathName
+
+    ' return an unused random number
+    Function GetUniqueRandomNumber() As Integer
+        Dim objRandom As Random = New Random
+        Dim intRandom As Integer
+        ' generate random numbers until unused flag is found
+        Do
+            intRandom = objRandom.Next(0, m_blnUsed.Length)
+        Loop Until m_blnUsed(intRandom) = False
+        ' indicate that flag has been used
+        m_blnUsed(intRandom) = True
+        Return intRandom  ' return index for new flag
+    End Function ' GetUniqueRandomNumber
+
+    ' display random flag in PictureBox
+    Sub DisplayFlag()
+        ' unique index ensures that a flag is used no more than once
+        Dim intRandom As Integer = GetUniqueRandomNumber()
+        ' retrieve country name from array m_strOptions
+        m_strCountry = m_strOptions(intRandom)
+        ' get images's full path name
+        Dim strPath As String = BuildPathName(m_strCountry)
+        Try
+            If IO.File.Exists(strPath) Then
+                flagPictureBox.Image = Image.FromFile(strPath)  ' display images
+            Else
+                MessageBox.Show("Image file not found: " & strPath, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("An error occurred while loading the image: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub ' DisplayFlag
+
+    ' handles Submit Button's Click event
+    Private Sub SubmitButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SubmitButton.Click
+        ' retrieve answer from ComboBox
+        Dim strResponse As String = Convert.ToString(OptionsComboBox.SelectedValue)
+        ' verify answer
+        If strResponse = m_strCountry Then
+            FeedbackLabel.Text = "Correct!"
+        Else
+            FeedbackLabel.Text = "Sorry, incorrect."
+        End If
+        ' inform user if quiz is over
+        If m_intCount >= 5 Then  ' quiz is over
+            FeedbackLabel.Text &= "   Done!"
+            NextButton.Enabled = False
+            SubmitButton.Enabled = False
+            OptionsComboBox.Enabled = False
+        Else  ' quiz is not over
+            SubmitButton.Enabled = False
+            NextButton.Enabled = True
+        End If
+    End Sub ' btnSubmit_Click
+
+    ' handles Next Flag Button's Click event
+    Private Sub NextButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NextButton.Click
+        DisplayFlag()  ' display next flag
+        FeedbackLabel.Text = ""  ' clear output
+        ' change selected country to first in ComboBox
+        OptionsComboBox.SelectedIndex = 0
+        m_intCount += 1  ' update number of flags shown
+        SubmitButton.Enabled = True
+        NextButton.Enabled = False
+    End Sub ' btnNext_Click
+
+    Private Sub OptionsComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles OptionsComboBox.SelectedIndexChanged
+
+    End Sub
+End Class ' Form1
